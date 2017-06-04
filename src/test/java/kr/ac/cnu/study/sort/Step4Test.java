fb5aff2cb7af8655c4775a1029487ab4f1579c7d
rokim <rokim@riotgames.com>
2017-06-04 17:47:26 +0900

    Modify Step4Test for template

--- src/test/java/kr/ac/cnu/study/sort/Step4Test.java
+++ src/test/java/kr/ac/cnu/study/sort/Step4Test.java
@@ -1,6 +1,10 @@
 package kr.ac.cnu.study.sort;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -8,7 +12,23 @@ import static org.junit.Assert.*;
  * Created by rokim on 2017. 6. 4..
  */
 public class Step4Test {
+    private List<Integer> list;
+
+    @Before
+    public void before() {
+        list = Arrays.asList(3, 5, 2, 4, 1);
+    }
+
     @Test
     public void isSwap() {
     }
+
+    @Test
+    public void 오름차순_테스트() {
+    }
+
+    @Test
+    public void 내림차순_테스트() {
+    }
+
 }
