fd6d900b9ee0efe0dad446d805f561b4024c4d3b
Dong-hee Na <donghee.na92@gmail.com>
2017-06-06 13:14:41 +0900

    [16조] 게시판 삭제 기능 구현 (WIP). (#3)

--- src/main/java/kr/ac/cnu/repository/BoardRepository.java
+++ src/main/java/kr/ac/cnu/repository/BoardRepository.java
@@ -1,11 +1,14 @@
 package kr.ac.cnu.repository;
 
 import kr.ac.cnu.domain.Board;
+import kr.ac.cnu.domain.CnuUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by rokim on 2017. 5. 31..
  */
 public interface BoardRepository extends JpaRepository<Board, Integer>{
     Board findByIdx(int idx);
+    Board findByIdxAndCnuUser(int idx, CnuUser cnuUser);
 }
