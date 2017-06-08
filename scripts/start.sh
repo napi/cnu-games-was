b9dba7872ac32bcb7d0ede29baaf7c1a692facb8
napi <rokim@riotgames.com>
2017-06-08 18:33:53 +0900

    Add script files for executing java application (#9)

---
+++ scripts/start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if ! [ -d /var/log/riot ];
+then
+  sudo mkdir /var/log/riot
+fi
+if ! [ -d /home/ec2-user/$1 ];
+then
+  sudo mkdir /home/ec2-user/$1
+fi
+cp -f $WORKSPACE/target/*.jar /home/ec2-user/$1
+cd /home/ec2-user/$1
+sudo /usr/bin/nohup /usr/bin/java -Xmx128M -jar -Dapplication.name=$1 /home/ec2-user/$1/*.jar > /dev/null 2> /dev/null < /dev/null &
